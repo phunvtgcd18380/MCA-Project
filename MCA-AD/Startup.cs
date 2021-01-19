f72ec194329ccdd0e527b4b4a1f40131a3bbd588
phunvtgcd18380 <66460872+phunvtgcd18380@users.noreply.github.com>
2021-01-19 16:17:03 +0700

    Add project files.

---
+++ MCA-AD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MCA_AD.Startup))]
+namespace MCA_AD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
